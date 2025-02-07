306060f70cb55ac5b5039f1417ce760c14b8566e
TheYolk34 <yellowyolk34@gmail.com>
2025-02-08 01:42:02 +0300

    pwa

--- pharma/dev-dist/sw.js
+++ pharma/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.uol52qt1cag"
+    "revision": "0.2qgpfjhf9r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
